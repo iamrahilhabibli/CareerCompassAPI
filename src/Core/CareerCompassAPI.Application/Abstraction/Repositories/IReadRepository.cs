9423236fed319ed8ba3ebbbb29f743ef188754f2
Rahil Habibli <rahilah@code.edu.az>
2023-08-07 20:04:23 +0400

    IReadRepository and IWriteRepository Implemented:

---
+++ src/Core/CareerCompassAPI.Application/Abstraction/Repositories/IReadRepository.cs
@@ -0,0 +1,23 @@
+﻿using CareerCompassAPI.Domain.Entities.Common;
+using System.Linq.Expressions;
+
+namespace CareerCompassAPI.Application.Abstraction.Repositories
+{
+    public interface IReadRepository<T>:IRepository<T> where T : BaseEntity, new()
+    {
+        IQueryable<T> GetAll(bool isTracking = true, params string[] includes);
+        IQueryable<T> GetAllByExpression(Expression<Func<T, bool>> expression,
+                                         int take,
+                                         int skip,
+                                         bool isTracking = true,
+                                         params string[] includes);
+        IQueryable<T> GetAllFilteredOrderedBy(Expression<Func<T, bool>> expression,
+                                              int take,
+                                              int skip,
+                                              Expression<Func<T, bool>> orderExpression,
+                                              bool isOrdered = true,
+                                              bool isTracking = true);
+        Task<T> GetByIdAsync(Guid Id);
+        Task<T> GetByExpressionAsync(Expression<Func<T, bool>> expression, bool isTracking = true);
+    }
+}
---
+++ src/Core/CareerCompassAPI.Application/Abstraction/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using CareerCompassAPI.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace CareerCompassAPI.Application.Abstraction.Repositories
+{
+    public interface IRepository<T> where T : BaseEntity, new()
+    {
+        public DbSet<T> Table { get; }
+    }
+}
